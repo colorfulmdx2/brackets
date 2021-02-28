f6e2270589ef54a523bb9382a88afbcdeca3c978
Viktor Berezovskyi <64471555+colorfulmdx2@users.noreply.github.com>
2021-02-28 21:48:40 +0300

    feat: end up check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newBracketsConfig = bracketsConfig.flatMap(element => element.join(''))
+
+    for (let i = 0; i < newBracketsConfig.length; i++) {
+        if (str.includes(newBracketsConfig[i])) {
+            str = str.replace(newBracketsConfig[i], '')
+            i--
+        }
+    }
+
+    return str === ''
 }
